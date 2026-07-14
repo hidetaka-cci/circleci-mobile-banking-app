729bbe3b25526583675bc6596be43327e86ce82a
Hidetaka Okamoto CCI <hidetaka.okamoto@circleci.com>
2026-07-14 14:29:43 +0900

    p1/a-3: unused var in payments/fees.js

--- miniapps/payments/src/lib/fees.js
+++ miniapps/payments/src/lib/fees.js
@@ -1,4 +1,5 @@
 // Card processing fee: 2.9% of the amount plus a flat 30c, rounded to cents.
+const P1_UNUSED = 'p1-a3';
 
 export function cardFeeCents(amountCents) {
   return Math.round(amountCents * 0.029) + 30;
